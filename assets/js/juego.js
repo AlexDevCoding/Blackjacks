0272366322264f30f645b1e83dbac92da6b3f374
Josue Alexander Henriquez <henriquezjosue384@gmail.com>
2024-04-25 20:22:00 -0400

    primer commit

---
+++ assets/js/juego.js
@@ -0,0 +1,50 @@
+/**
+ * 2C = Two of Clubs 
+ * 2D = Two of Diaminds
+ * 2H = Two of Hearts 
+ * 2S = Two of Spades
+ */
+
+let deck = [];
+const tipos = ["C", "D", "H", "S"];
+const especiales = ["A","J","Q","K"];
+
+const crearDeck = () => {
+
+    for(let i =2; i <= 10; i++) {
+     for(let tipo of tipos) {
+        deck.push(i+tipo);
+     }
+    }
+
+    for(let tipo of tipos) {
+        for(let esp of especiales) {
+            deck.push(esp+tipo);
+        }
+    }
+    // console.log(deck);
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck
+}
+
+crearDeck();
+
+// Esta Función me permite tomar una carta
+const pedirCarta = () => {
+
+    if (deck.length === 0){
+        throw "No hay cartas en el deck";
+    } 
+
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); // carta debe de ser de la baraja 
+    return carta;
+}  
+
+
+    // pedirCarta();s
+
