6a3c38095954426449fec33176f86ccbeb3cf147
Josue Alexander Henriquez <henriquezjosue384@gmail.com>
2024-04-27 23:51:31 -0400

    segundo commit

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -43,8 +43,17 @@ const pedirCarta = () => {
     console.log(deck);
     console.log(carta); // carta debe de ser de la baraja 
     return carta;
-}  
-
-
+} 
     // pedirCarta();s
 
+const valorCarta = (carta) => {
+
+    const valor = carta.substring(0, carta.length -1);
+    return (isNaN(valor))?
+        (valor ==="A") ? 11:10
+        :valor *1;
+    
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({valor});
